3d838543faffc038e705976b240f754d3648fa77
João C <analista.jcarloslima@gmail.com>
2020-12-02 16:36:56 -0300

    aplicaçao de ip corrigida

--- 17-Fix aplicação/Main.go
+++ 17-Fix aplicação/Main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Switch")
-
+	aplicacao := app.Gerar()
+	if erro := aplicacao.Run(os.Args); erro != nil {
+		log.Fatal(erro)
+	}
 }
-
-// só testando a alteração
